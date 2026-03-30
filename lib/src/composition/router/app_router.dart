e05ef29cb372e8ca51f121250abe325c88ad60f5
Romulo Sanchez <sanchez.systems.24@gmail.com>
2026-03-29 20:05:33 -0600

    Update app_router.dart

--- lib/src/composition/router/app_router.dart
+++ lib/src/composition/router/app_router.dart
@@ -8,33 +8,6 @@ import 'package:works_fm/src/featues/splash/screens/error_screen.dart';
 import 'package:works_fm/src/featues/splash/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-/* class AppRouter {
-  final GetIt it;
-  AppRouter(this.it);
-
-  late final GoRouter router = GoRouter(
-    initialLocation: AppRouterPaths.splash,
-    redirect: _redirect,
-    routes: [
-      GoRoute(path: AppRouterPaths.splash,
-      builder: (context, state) => BlocProvider(
-        create: (context) => it<SplashBloc>(),
-        child: const SplashScreen(),
-      )),
-      ...featureModules.expand((f)=>f.routes())
-    ],
-    observers: [
-      it<RouteObserver<ModalRoute<void>>>()
-    ],
-    errorBuilder: (context, state) => ErrorScreen(errorMsg: state.error!.message),
-  );
-
-  String? _redirect(BuildContext context, GoRouterState state) {
-    return null;
-  }
-} */
-
-// lib/src/composition/router/app_router.dart
 class AppRouter {
   final GetIt it;
   AppRouter(this.it);
