d21d20cea42c7061c51a21950814f26aca535235
Romulo Sanchez <sanchez.systems.24@gmail.com>
2026-04-01 14:10:57 -0600

    Add core library by github repository

--- packages/core/lib/di/feature_module.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:go_router/go_router.dart';
-
-abstract class FeatureModule {
-  void register(GetIt it);
-  List<RouteBase> routes();
-  List<LocalizationsDelegate> get delegates => [];
-}
--- packages/core/lib/utils/typedefs.dart
+++
@@ -1,5 +0,0 @@
-import 'package:core/error/failures.dart';
-import 'package:fpdart/fpdart.dart';
-
-typedef ResultFuture<T> = TaskEither<Failure, T>;
-typedef ResultVoid = TaskEither<Failure, Unit>;
